9585eea63b34bd30775a03b853cd4271b2dfdb07
cznull <32773496+cznull@users.noreply.github.com>
2020-11-23 13:52:50 +0800

    Update branchedflowp.m

--- branchedflowp.m
+++ branchedflowp.m
@@ -6,15 +6,15 @@ r=real(ifft2(m));
 
 V=r*0.05;
 u=(1:n)'-n/2-0.5;
-u=exp(-(16.0/8192)*u.*u);
+u=exp(-(16.0/8192)*u.*u);%incident light
 
 par=zeros(n,n);
 
-k=2*pi*8192;
+k=2*pi*4096;%4096: 1/wave length
 T=(2*pi*pi/k)*x.*x;
 for i=1:n
     u=fft(u);
-    u=u.*exp(-1i*(1.0/n)*T);
+    u=u.*exp(1i*(1.0/n)*T);
     u=ifft(u);
     u=u.*exp(-1i*(k/n/2)*V(:,i));
     par(:,i)=u;
@@ -22,4 +22,6 @@ end
 imshow(abs(par),[0,2]);
 end
 
-//[r,par]=branchedflowp(32,4096);
+%example: branchedflowp(64,4096);
+%4096: grid size
+%64: 1/correlation length
